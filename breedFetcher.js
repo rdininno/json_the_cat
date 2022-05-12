314a4d94003888795c1c11ac3dae94a8d2b86ad9
Robert Di Ninno <robertdininno@gmail.com>
2022-05-12 16:41:01 +0000

    refactor to use callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,16 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(" ERROR: ", error);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    let data = JSON.parse(body);
 
-  let data = JSON.parse(body);
-  if (data[0] !== undefined) {
-    console.log("Description: ", data[0].description);
-  } else {
-    console.log("Breed not found");
-  }
-});
+    if (data[0] !== undefined) {
+      callback(null, data[0].description);
+    } else {
+      callback(error, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
